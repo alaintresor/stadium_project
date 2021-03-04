9e5f51af2a292430c170c17736aa515e410adab9
Tresor <tresoralain35@gmail.com>
2021-03-05 00:59:40 +0200

    updates

--- admin/manage_postpone_management.php
+++ admin/manage_postpone_management.php
@@ -41,60 +41,45 @@ if (isset($_POST['submit'])) {
             // updating date on fixtures
             mysqli_query($con, "UPDATE `fixtures` SET `date`='$movedOn',`time`='$movedAt',`status`='postponed' WHERE `id`='$fixtureId'");
         } else {
-            $selp=mysqli_query($con,"SELECT * FROM booking_teckets WHERE fixture_id='$fixtureId'");
-            while($row=mysqli_fetch_array($selp)){
-                $sel=$con->query("SELECT telephone FROM customers WHERE id='".$row['1']."'");
-                $res=mysqli_fetch_array($sel);
-                      	// Account details
-	$apiKey = urlencode('QCZCGpsNseI-gd0snkBj4zC49shTyTbP1AAW9AgNMX');
-	
-	// Message details
-	$numbers = array($res[0], );
-	$sender = urlencode('Smart Ticket System');
-	$message = rawurlencode('This is your message');
- 
-	$numbers = implode(',', $numbers);
- 
-	// Prepare data for POST request
-	$data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
- 
-	// Send the POST request with cURL
-	$ch = curl_init('https://api.txtlocal.com/send/');
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-	curl_close($ch);
-	
-	// Process your response here
-	echo $response;
-            }
-    //         	// Account details
-	// $apiKey = urlencode('QCZCGpsNseI-gd0snkBj4zC49shTyTbP1AAW9AgNMX');
-	
-	// // Message details
-	// $numbers = array(, );
-	// $sender = urlencode('Smart Ticket System');
-	// $message = rawurlencode('This is your message');
- 
-	// $numbers = implode(',', $numbers);
- 
-	// // Prepare data for POST request
-	// $data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
- 
-	// // Send the POST request with cURL
-	// $ch = curl_init('https://api.txtlocal.com/send/');
-	// curl_setopt($ch, CURLOPT_POST, true);
-	// curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	// $response = curl_exec($ch);
-	// curl_close($ch);
-	
-	// // Process your response here
-	// echo $response;
-            mysqli_query($con, "INSERT INTO `postponed_matchs` (`id`, `fixture_id`, `reason`,`fromOn`,`fromAt`, `moved_date`, `moved_time`) VALUES (NULL, '$fixtureId', '$reason','$fromOn[0]','$fromAt[0]', '$movedOn', '$movedAt')");
+            $done = mysqli_query($con, "INSERT INTO `postponed_matchs` (`id`, `fixture_id`, `reason`,`fromOn`,`fromAt`, `moved_date`, `moved_time`) VALUES (NULL, '$fixtureId', '$reason','$fromOn[0]','$fromAt[0]', '$movedOn', '$movedAt')");
             // updating date on fixtures
             mysqli_query($con, "UPDATE `fixtures` SET `date`='$movedOn',`time`='$movedAt',`status`='postponed' WHERE `id`='$fixtureId'");
+            if ($done) {
+                $ticketsQuery = "SELECT customers.fullname,customers.telephone,fixtures.home_team,fixtures.away_team FROM customers,fixtures,booking_teckets WHERE customers.id=booking_teckets.customer_id AND booking_teckets.fixture_id=fixtures.id AND fixtures.id='$fixtureId'";
+
+                $ticketsData = mysqli_query($con, "$ticketsQuery");
+
+                while ($row = mysqli_fetch_array($ticketsData)) {
+                    $homeTeam = mysqli_fetch_array(mysqli_query($con, "SELECT `name` FROM `teams` WHERE `id`='{$row['home_team']}'"));
+
+                    $awayTeam = mysqli_fetch_array(mysqli_query($con, "SELECT `name` FROM `teams` WHERE `id`='{$row['away_team']}'"));
+
+                    $phone = "+25" . $row['telephone'];
+                    $message = "Dear {$row['fullname']} , your ticket has been postponed due to match({$homeTeam[0]} VS {$awayTeam[0]}) is postponed from {$fromOn[0]} at {$fromAt[0]}, to {$movedOn} at {$movedAt},  because {$reason}.
+                   
+                     THANK YOU
+                    ";
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://api.mista.io/sms',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => array('to' => $phone, 'from' => 'SmarTicket', 'unicode' => '0', 'sms' => $message, 'action' => 'send-sms'),
+                        CURLOPT_HTTPHEADER => array(
+                            'x-api-key:andrass7'
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+
+                    curl_close($curl);
+                }
+            }
         }
         header('location:postpone_management.php');
         die();
--- stripe/charge.php
+++ stripe/charge.php
@@ -1,76 +1,72 @@
 <?php
 include '../config.php';
-  require_once('../stripe/config.php');
+require_once('../stripe/config.php');
 
-  $token  = $_POST['stripeToken'];
-  $email  = $_POST['stripeEmail'];
+$token  = $_POST['stripeToken'];
+$email  = $_POST['stripeEmail'];
 
-  $customer = \Stripe\Customer::create([
-      'email' => $email,
-      'source'  => $token,
-  ]);
+$customer = \Stripe\Customer::create([
+  'email' => $email,
+  'source'  => $token,
+]);
 
-  $charge = \Stripe\Charge::create([
-      'customer' => $customer->id,
-      'amount'   => $_POST['amount'],
-      'currency' => 'rwf',
-  ]);
-   $day=date("d");
-   $month=date("m");
-   $year=2000+date("y");
-   $num = rand(9999999,100000);
-   $tckCode="RNTC".$num;
-   $sele=$con->query("SELECT * FROM customers WHERE id='".$_POST['user_id']."'");
-   $res1=mysqli_fetch_array($sele);
-   $sel2=$con->query("SELECT * FROM fixtures WHERE id='".$_POST['fix_id']."'");
-   $fix = mysqli_fetch_array($sel2);
-   $selt = $con->query("SELECT name,logo FROM teams WHERE id='" . $fix['home_team'] . "'");
-   $selt2 = $con->query("SELECT name,logo FROM teams WHERE id='" . $fix['away_team'] . "'");
-   $rest = mysqli_fetch_array($selt);
-   $rest2 = mysqli_fetch_array($selt2);
-   //update if payfull successed
-   $insert=mysqli_query($con,"INSERT INTO booking_teckets(id,customer_id,fixture_id,seat,n_of_seats,amount) VALUES('$tckCode','".$_POST['user_id']."','".$_POST['fix_id']."','".$_POST['cat']."','".$_POST['seats']."','".$_POST['amount']."') "); 
-   $category=$_POST['cat'];
-   if( $category=='vip'){
-    $update_seat=$con->query("UPDATE seats_and_prices SET vip_seats=vip_seats-'".$_POST['seats']."' WHERE fixture_id='".$_POST['fix_id']."'");
-   }else if( $category=='vvip'){
-    $update_seat=$con->query("UPDATE seats_and_prices SET vvip_seats=vvip_seats-'".$_POST['seats']."' WHERE fixture_id='".$_POST['fix_id']."'");
-   }else if($category=='roofed'){
-    $update_seat=$con->query("UPDATE seats_and_prices SET roofed_seats=roofed_seats-'".$_POST['seats']."' WHERE fixture_id='".$_POST['fix_id']."'");
-   }else{
-    $update_seat=$con->query("UPDATE seats_and_prices SET unroofed_seats=unroofed_seats-'".$_POST['seats']."' WHERE fixture_id='".$_POST['fix_id']."'");
-   }
+$charge = \Stripe\Charge::create([
+  'customer' => $customer->id,
+  'amount'   => $_POST['amount'],
+  'currency' => 'rwf',
+]);
+$day = date("d");
+$month = date("m");
+$year = 2000 + date("y");
+$num = rand(9999999, 100000);
+$tckCode = "RNTC$num";
+$sele = $con->query("SELECT * FROM customers WHERE id='" . $_POST['user_id'] . "'");
+$res1 = mysqli_fetch_array($sele);
+$sel2 = $con->query("SELECT * FROM fixtures WHERE id='" . $_POST['fix_id'] . "'");
+$fix = mysqli_fetch_array($sel2);
+$selt = $con->query("SELECT name,logo FROM teams WHERE id='" . $fix['home_team'] . "'");
+$selt2 = $con->query("SELECT name,logo FROM teams WHERE id='" . $fix['away_team'] . "'");
+$rest = mysqli_fetch_array($selt);
+$rest2 = mysqli_fetch_array($selt2);
+//update if payfull successed
+$inseryQuery = "INSERT INTO `booking_teckets` (`id`, `customer_id`, `fixture_id`, `seat`, `n_of_seats`, `amount`) VALUES ('$tckCode','{$_POST['user_id']}','{$_POST['fix_id']}','{$_POST['cat']}','{$_POST['seats']}','{$_POST['amount']}');";
+$insert = mysqli_query($con, "$inseryQuery");
+$category = $_POST['cat'];
+if ($category == 'vip') {
+  $update_seat = $con->query("UPDATE seats_and_prices SET vip_seats=vip_seats-'" . $_POST['seats'] . "' WHERE fixture_id='" . $_POST['fix_id'] . "'");
+} else if ($category == 'vvip') {
+  $update_seat = $con->query("UPDATE seats_and_prices SET vvip_seats=vvip_seats-'" . $_POST['seats'] . "' WHERE fixture_id='" . $_POST['fix_id'] . "'");
+} else if ($category == 'roofed') {
+  $update_seat = $con->query("UPDATE seats_and_prices SET roofed_seats=roofed_seats-'" . $_POST['seats'] . "' WHERE fixture_id='" . $_POST['fix_id'] . "'");
+} else {
+  $update_seat = $con->query("UPDATE seats_and_prices SET unroofed_seats=unroofed_seats-'" . $_POST['seats'] . "' WHERE fixture_id='" . $_POST['fix_id'] . "'");
+}
 
-   if ($insert) {
+if ($insert) {
+} else {
+  echo "not" . mysqli_error($con);
+}
 
+$phone = "+25" . $res1['telephone'];
+$message = "Dear " . $res1['fullname'] . ", your ticket reservation code is " . $tckCode . " for " . $rest[0] . " Vs " . $rest2[0] . " at " . $fix['location'] . " on " . $fix['date'] . ", " . $fix['time'] . ".  Amount paid: " . $_POST['amount'] . "   THANK YOU FOR BOOKING WITH US";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.mista.io/sms',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('to' => $phone, 'from' => 'SmarTicket', 'unicode' => '0', 'sms' => $message, 'action' => 'send-sms'),
+  CURLOPT_HTTPHEADER => array(
+    'x-api-key:andrass7'
+  ),
+));
 
-   }else{
-     echo "not".mysqli_error($con);
-   }
-   
- $phone = "+25".$res1['telephone'];
- $message= "Dear ".$res1['fullname'].", your ticket reservation code is".$tckCode." for ".$rest[0]." Vs ".$rest2[0]." at ".$fix['location']." on ".$fix['date'].", ".$fix['time'].".  Amount paid: ".$_POST['amount']."   THANK YOU FOR BOOKING WITH US";
-  $curl = curl_init();
- curl_setopt_array($curl, array(
-   CURLOPT_URL => 'https://api.mista.io/sms',
-   CURLOPT_RETURNTRANSFER => true,
-   CURLOPT_ENCODING => '',
-   CURLOPT_MAXREDIRS => 10,
-   CURLOPT_TIMEOUT => 0,
-   CURLOPT_FOLLOWLOCATION => true,
-   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-   CURLOPT_CUSTOMREQUEST => 'POST',
-   CURLOPT_POSTFIELDS => array('to' => $phone,'from' => 'SmarTicket','unicode' => '0','sms' => $message,'action' => 'send-sms'),
-   CURLOPT_HTTPHEADER => array(
-     'x-api-key:andrass7'
-   ),
- ));
- 
- $response = curl_exec($curl);
- 
- curl_close($curl);
- //echo "<center><script><script>history.go(-2)</script></center>";
- echo "<center><script>window.open();history.go(-2)</script></center>";
+$response = curl_exec($curl);
 
-  
-?>
+curl_close($curl);
+
+echo "<script>alert('Your ticket is booked successfully,Check your SMS on phone');window.open('../index.php','_self');</script>";
